4715655661b22edc2686eded284eecdb992539ee
Aegonian <25555407+Aegonian@users.noreply.github.com>
2017-12-05 16:38:05 +0100

    Unique Weapon Update
    -Finished a new Akkimari Unique weapon, Scrapyard (no collections entry yet)
    -Improvements to the Akkimari nailgun
    -Avikan weapon displays should no longer be uncraftable with the Universal Uncrafter mod

---
+++ items/active/weapons/ranged/abilities/thea-randomprojectilegunfire.lua
@@ -0,0 +1,143 @@
+require "/scripts/util.lua"
+require "/scripts/interp.lua"
+
+-- Base gun fire ability
+TheaRandomProjectileGunFire = WeaponAbility:new()
+
+function TheaRandomProjectileGunFire:init()
+  self.weapon:setStance(self.stances.idle)
+
+  self.cooldownTimer = self.fireTime
+
+  self.weapon.onLeaveAbility = function()
+    self.weapon:setStance(self.stances.idle)
+  end
+end
+
+function TheaRandomProjectileGunFire:update(dt, fireMode, shiftHeld)
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  self.cooldownTimer = math.max(0, self.cooldownTimer - self.dt)
+
+  if animator.animationState("firing") ~= "fire" then
+    animator.setLightActive("muzzleFlash", false)
+  end
+
+  if self.fireMode == (self.activatingFireMode or self.abilitySlot)
+    and not self.weapon.currentAbility
+    and self.cooldownTimer == 0
+    and not status.resourceLocked("energy")
+    and not world.lineTileCollision(mcontroller.position(), self:firePosition()) then
+
+    if self.fireType == "auto" and status.overConsumeResource("energy", self:energyPerShot()) then
+      self:setState(self.auto)
+    elseif self.fireType == "burst" then
+      self:setState(self.burst)
+    end
+  end
+end
+
+function TheaRandomProjectileGunFire:auto()
+  self.weapon:setStance(self.stances.fire)
+
+  self:fireProjectile()
+  self:muzzleFlash()
+
+  if self.stances.fire.duration then
+    util.wait(self.stances.fire.duration)
+  end
+
+  self.cooldownTimer = self.fireTime
+  self:setState(self.cooldown)
+end
+
+function TheaRandomProjectileGunFire:burst()
+  self.weapon:setStance(self.stances.fire)
+
+  local shots = self.burstCount
+  while shots > 0 and status.overConsumeResource("energy", self:energyPerShot()) do
+    self:fireProjectile()
+    self:muzzleFlash()
+    shots = shots - 1
+
+    self.weapon.relativeWeaponRotation = util.toRadians(interp.linear(1 - shots / self.burstCount, 0, self.stances.fire.weaponRotation))
+    self.weapon.relativeArmRotation = util.toRadians(interp.linear(1 - shots / self.burstCount, 0, self.stances.fire.armRotation))
+
+    util.wait(self.burstTime)
+  end
+
+  self.cooldownTimer = (self.fireTime - self.burstTime) * self.burstCount
+end
+
+function TheaRandomProjectileGunFire:cooldown()
+  self.weapon:setStance(self.stances.cooldown)
+  self.weapon:updateAim()
+
+  local progress = 0
+  util.wait(self.stances.cooldown.duration, function()
+    local from = self.stances.cooldown.weaponOffset or {0,0}
+    local to = self.stances.idle.weaponOffset or {0,0}
+    self.weapon.weaponOffset = {interp.linear(progress, from[1], to[1]), interp.linear(progress, from[2], to[2])}
+
+    self.weapon.relativeWeaponRotation = util.toRadians(interp.linear(progress, self.stances.cooldown.weaponRotation, self.stances.idle.weaponRotation))
+    self.weapon.relativeArmRotation = util.toRadians(interp.linear(progress, self.stances.cooldown.armRotation, self.stances.idle.armRotation))
+
+    progress = math.min(1.0, progress + (self.dt / self.stances.cooldown.duration))
+  end)
+end
+
+function TheaRandomProjectileGunFire:muzzleFlash()
+  animator.setPartTag("muzzleFlash", "variant", math.random(1, 3))
+  animator.setAnimationState("firing", "fire")
+  animator.burstParticleEmitter("muzzleFlash")
+  animator.playSound("fire")
+
+  animator.setLightActive("muzzleFlash", true)
+end
+
+function TheaRandomProjectileGunFire:fireProjectile(projectileType, projectileParams, inaccuracy, firePosition, projectileCount)
+  local params = sb.jsonMerge(self.projectileParameters, projectileParams or {})
+  params.power = self:damagePerShot()
+  params.powerMultiplier = activeItem.ownerPowerMultiplier()
+  params.speed = util.randomInRange(params.speed)
+
+  local projectileId = 0
+  for i = 1, (projectileCount or self.projectileCount) do
+    if params.timeToLive then
+      params.timeToLive = util.randomInRange(params.timeToLive)
+    end
+
+	randomProjectileType = self.projectileType[math.random(#self.projectileType)]
+	
+    projectileId = world.spawnProjectile(
+        randomProjectileType,
+        firePosition or self:firePosition(),
+        activeItem.ownerEntityId(),
+        self:aimVector(inaccuracy or self.inaccuracy),
+        false,
+        params
+      )
+  end
+  return projectileId
+end
+
+function TheaRandomProjectileGunFire:firePosition()
+  return vec2.add(mcontroller.position(), activeItem.handPosition(self.weapon.muzzleOffset))
+end
+
+function TheaRandomProjectileGunFire:aimVector(inaccuracy)
+  local aimVector = vec2.rotate({1, 0}, self.weapon.aimAngle + sb.nrand(inaccuracy, 0))
+  aimVector[1] = aimVector[1] * mcontroller.facingDirection()
+  return aimVector
+end
+
+function TheaRandomProjectileGunFire:energyPerShot()
+  return self.energyUsage * self.fireTime * (self.energyUsageMultiplier or 1.0)
+end
+
+function TheaRandomProjectileGunFire:damagePerShot()
+  return (self.baseDamage or (self.baseDps * self.fireTime)) * (self.baseDamageMultiplier or 1.0) * config.getParameter("damageLevelMultiplier") / self.projectileCount
+end
+
+function TheaRandomProjectileGunFire:uninit()
+end
--- projectiles/scripts/thea-randomspeedprojectile.lua
+++ projectiles/scripts/thea-randomspeedprojectile.lua
@@ -8,6 +8,17 @@ function init()
   local currentVelocity = mcontroller.velocity()
   local newVelocity = vec2.mul(vec2.norm(currentVelocity), targetSpeed)
   mcontroller.setVelocity(newVelocity)
+  
+  if config.getParameter("randomTimeToLive") == true then
+	local minLifeTime = config.getParameter("minLifeTime")
+	local maxLifeTime = config.getParameter("maxLifeTime")
+	local lifeTime = math.random(minLifeTime, maxLifeTime)
+	if config.getParameter("timeToLiveMilliseconds") == true then
+	  projectile.setTimeToLive(lifeTime/100)
+	else
+	  projectile.setTimeToLive(lifeTime)
+	end
+  end
 end
 
 function update()
