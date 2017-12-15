3413bb726370307ca2683e9c74414256c6e48bea
Aegonian <25555407+Aegonian@users.noreply.github.com>
2017-12-15 18:11:29 +0100

    Akkimari Weapon Updates
    -Added a new type of liquid, Vaash (AKA Akkimari Acid)
    -Added a new type of status effect, Vaash Burn
    -Enemies killed while under the effect of Vaash Burn leave behind a smoking pile of ash
    -Added a new elemental and damage type, Vaash
    -Some Akkimari weapons now inflict Vaash Burn

--- projectiles/scripts/thea-uprightprojectile.lua
+++ projectiles/scripts/thea-uprightprojectile.lua
@@ -1,8 +1,6 @@
 require "/scripts/vec2.lua"
 
 function init()
-  self.rotateInAir = config.getParameter("rotateInAir")
-  
   if config.getParameter("randomTimeToLive") == true then
 	local minLifeTime = config.getParameter("minLifeTime")
 	local maxLifeTime = config.getParameter("maxLifeTime")
@@ -20,8 +18,11 @@ function update(dt)
     mcontroller.setRotation(0)
     self.hitGround = true
   else
-    if self.rotateInAir and self.rotateInAir == true then
+    if config.getParameter("rotateInAir") then
       mcontroller.setRotation(math.atan(mcontroller.velocity()[2], mcontroller.velocity()[1]))
+	end
+	if config.getParameter("alwaysUpright") then
+      mcontroller.setRotation(0)
 	end
   end
 end
---
+++ stats/effects/akkimariacidburn/akkimariacidburn.lua
@@ -0,0 +1,57 @@
+require "/scripts/util.lua"
+
+function init()
+  if status.resourceMax("health") < config.getParameter("minMaxHealth", 0) then
+    effect.expire()
+  end
+  
+  animator.setParticleEmitterOffsetRegion("flames", mcontroller.boundBox())
+  animator.setParticleEmitterActive("flames", true)
+  effect.setParentDirectives(config.getParameter("directive"))
+  
+  script.setUpdateDelta(5)
+
+  self.tickDamagePercentage = 0.025
+  self.tickTime = 0.75
+  self.tickTimer = self.tickTime
+end
+
+function update(dt)
+
+  self.tickTimer = self.tickTimer - dt
+  
+  --Actions to be performed on every tick
+  if self.tickTimer <= 0 then
+    self.tickTimer = self.tickTime
+	
+	--Play the damage sound
+	animator.playSound("shock")
+	
+	local targetDamage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1
+	local actualDamage = math.min(targetDamage, 25)
+	
+	--Apply damage to self
+    status.applySelfDamageRequest({
+	  damageType = "IgnoresDef",
+	  damage = actualDamage,
+	  damageSourceKind = config.getParameter("damageKind"),
+	  sourceEntityId = entity.id()
+	})
+  end
+  
+  if not status.resourcePositive("health") and status.resourceMax("health") >= config.getParameter("minMaxHealth", 0) then
+    effect.setParentDirectives(config.getParameter("deathDirective"))
+	explode()
+  end
+end
+
+function explode()
+  if not self.exploded then
+    world.spawnProjectile(config.getParameter("deathProjectileType"), mcontroller.position(), 0, {0, 0}, false)
+    self.exploded = true
+  end
+end
+
+function uninit()
+  
+end
