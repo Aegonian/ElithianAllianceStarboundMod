7ea4c9c290f795b5d2abc1b0f290331f528715a3
Aegonian <25555407+Aegonian@users.noreply.github.com>
2017-12-31 18:40:49 +0100

    Various Updates
    -Added three new monsters (Justicar, Arbiter and Flic)
    -Added collectable figurines for the new monsters
    -Added captured monster collection entries for the new monsters
    -Akkimari can now be spawned in as NPCs, but dialog is missing
    -Added tiered crafting materials for the Akkimari
    -Akkimari acid projectiles now have a unique impact effect
    -Added a new Interface Wall material for Alliance races
    -Added new Alliance Interface objects to go along with the new Interface Wall
    -Added various new Alliance-themed wired objects
    -Small drones will now spawn custom debris particles when killed
    -Other minor changes

--- projectiles/scripts/thea-proximityexplode.lua
+++ projectiles/scripts/thea-proximityexplode.lua
@@ -2,6 +2,7 @@ require "/scripts/vec2.lua"
 
 function init()
   self.searchDistance = config.getParameter("searchRadius")
+  self.searchDelayAfterSticky = config.getParameter("searchDelayAfterSticky") or false
   
   if config.getParameter("searchStartDelay") ~= nil then
 	self.searchEnabled = false
@@ -18,9 +19,10 @@ function init()
 end
 
 function update(dt)
-  if not self.proximityEnabled then
+  if not self.collided then
 	if projectile.collision() or mcontroller.isCollisionStuck() or mcontroller.isColliding() then
 	  self.proximityEnabled = true
+	  self.collided = true
 	end
   end
   
@@ -40,9 +42,22 @@ function update(dt)
 	  end
 	end
   else
-	self.countdownTimer = math.max(0, self.countdownTimer - dt)
-	if self.countdownTimer == 0 then
-	  self.searchEnabled = true
+	if self.searchDelayAfterSticky and self.collided then
+	  self.countdownTimer = math.max(0, self.countdownTimer - dt)
+	  if self.countdownTimer == 0 then
+		self.searchEnabled = true
+	  end
+	elseif not self.searchDelayAfterSticky then
+	  self.countdownTimer = math.max(0, self.countdownTimer - dt)
+	  if self.countdownTimer == 0 then
+		self.searchEnabled = true
+	  end
+	end
+  end
+  
+  if config.getParameter("killOnParentDeath") and self.searchEnabled then
+	if not world.entityExists(projectile.sourceEntity()) then
+	  projectile.die()
 	end
   end
 end
