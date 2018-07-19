9258d61fc3145bd5ea4a72c2217e47703e78dc07
Aegonian <25555407+Aegonian@users.noreply.github.com>
2018-07-19 14:17:41 +0200

    Various Updates
    -Added a Trinkian Rail Pod
    -Added Droden Watcher armour
    -Added a simple quest for obtaining the Droden Watcher armour
    -Fixed the Alliance hologram previews not flipping correctly
    -Fixed Excalibur not dealing Xanafian damage
    -Fixed Aegi recruited from the Recruitment Desk not updating their outfit when the tailor is used
    -Fixed Alliance holograms and interfaces repositioning if turned on after they were turned off and the game was quit and saved
    -Medical kits now remove Vaash Burn
    -Alliance Antenna variations can now be placed on ceilings
    -Trinkian NPCs are now immune to bleeding effects
    -Aded a crafting recipe for the Aegi Ship Light
    -Alliance Sewage Bars now drop as an item when broken
    -Added Human and Protectorate refugees to the Creon Embassy

--- objects/alliance/other/alliancerecruitmentdesk/alliancerecruitmentdesk.lua
+++ objects/alliance/other/alliancerecruitmentdesk/alliancerecruitmentdesk.lua
@@ -17,21 +17,39 @@ function spawnCompanion(_, _, crewmemberType)
 		uniqueId = uniqueId
 	  }
 	}
+  local entitySpecies = util.randomFromList(config.getParameter("crewmemberSpecies", {"aegi"}))
   local entityType = "crewmemberalliance"
   if crewmemberType == "combat" then
 	entityType = "crewmemberalliancecombat"
+	if entitySpecies == "aegi" then
+	  entityType = "crewmemberalliancecombat-aegi"
+	end
   elseif crewmemberType == "medic" then
 	entityType = "crewmemberalliancemedic"
+	if entitySpecies == "aegi" then
+	  entityType = "crewmemberalliancemedic-aegi"
+	end
   elseif crewmemberType == "engineer" then
 	entityType = "crewmemberallianceengineer"
+	if entitySpecies == "aegi" then
+	  entityType = "crewmemberallianceengineer-aegi"
+	end
   elseif crewmemberType == "mechanic" then
 	entityType = "crewmemberalliancemechanic"
+	if entitySpecies == "aegi" then
+	  entityType = "crewmemberalliancemechanic-aegi"
+	end
   elseif crewmemberType == "janitor" then
 	entityType = "crewmemberalliancejanitor"
+	if entitySpecies == "aegi" then
+	  entityType = "crewmemberalliancejanitor-aegi"
+	end
   elseif crewmemberType == "tailor" then
 	entityType = "crewmemberalliancetailor"
+	if entitySpecies == "aegi" then
+	  entityType = "crewmemberalliancetailor-aegi"
+	end
   end
-  local entitySpecies = util.randomFromList(config.getParameter("crewmemberSpecies", {"aegi"}))
   local entityId = world.spawnNpc(vec2.add(entity.position(), config.getParameter("spawnOffset", {0,0})), entitySpecies, entityType, world.threatLevel(), nil, parameters)
   world.callScriptedEntity(entityId, "status.addEphemeralEffect", "beamin")
 end
---
+++ stats/effects/akkimariacidblock/akkimariacidblock.lua
@@ -0,0 +1,13 @@
+function init()
+   effect.addStatModifierGroup({{stat = "akkimariacidResistance", amount = 0.25}, {stat = "akkimariacidStatusImmunity", amount = 1}})
+
+   script.setUpdateDelta(0)
+end
+
+function update(dt)
+
+end
+
+function uninit()
+  
+end
