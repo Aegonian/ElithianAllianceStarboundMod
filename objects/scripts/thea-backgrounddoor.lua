527c6a56c328abf1e330d10d904d18d90f9dcede
Aegonian <25555407+Aegonian@users.noreply.github.com>
2019-01-10 17:51:36 +0100

    Various Fixes
    -Fixed collision issues with Akkimari Hammocks
    -Hammocks are now one tile taller and hang lower to prevent player from getting stuck after reloading
    -Fixed some minor issues with the background doors
    -Added sounds and lock mechanisms to background doors
    -Added more background door objects
    -Background door teleportation will now fail if the user is in an active movement ability
    -Fixed cover images on Akkimari loungable chairs

--- objects/scripts/thea-backgrounddoor.lua
+++ objects/scripts/thea-backgrounddoor.lua
@@ -7,20 +7,35 @@ function init()
   self.detectArea[2] = object.toAbsolutePosition(self.detectArea[2])
   
   self.connectedDoor = nil
-  self.connectionChecked = false
+  self.doorLocked = false
+  self.lockInputLevel = nil
+  self.forceOpenTimer = 0
   
   message.setHandler("openDoor", function()
 	if animator.animationState("doorState") == "closed" or animator.animationState("doorState") == "closing" then
-	  animator.setAnimationState("doorState", "opening")
+	  animator.setAnimationState("doorState", "open")
+	  self.forceOpenTimer = 0.5
 	end
   end)
 end
 
 function update(dt)
-  if not self.connectedDoor and not self.connectionChecked then
+  --If we don't have a connected door, check for one every frame
+  --Doing this every frame prevents rare issues where the connection check fails when loading a new sector
+  if not self.connectedDoor then
 	checkDoorConnection()
-	self.connectionChecked = true
   end
+  
+  --If we have input on the lock node, invert that to set lock state. If there is no connection in the lock node, unlock the door
+  if object.isInputNodeConnected(1) then
+	self.lockInputLevel = object.getInputNodeLevel(1)
+	self.doorLocked = not self.lockInputLevel
+  else
+	self.doorLocked = false
+  end
+  
+  --Count down the force open timer
+  self.forceOpenTimer = math.max(0, self.forceOpenTimer - dt)
   
   --Check if there are players nearby
   local players = world.entityQuery(self.detectArea[1], self.detectArea[2], {
@@ -29,17 +44,23 @@ function update(dt)
   })
 
   --If the door is connected, open or close the door based on player proximity
-  if #players > 0 and self.connectedDoor and animator.animationState("doorState") == "closed" then
+  if #players > 0 and self.connectedDoor and animator.animationState("doorState") == "closed" and not self.doorLocked then
 	animator.setAnimationState("doorState", "opening")
-  elseif #players == 0 and self.connectedDoor and animator.animationState("doorState") == "open" then
+	animator.playSound("open")
+  elseif #players == 0 and self.connectedDoor and animator.animationState("doorState") == "open" and self.forceOpenTimer == 0 and not self.doorLocked then
 	animator.setAnimationState("doorState", "closing")
+	animator.playSound("close")
   --If the door isn't connected, try to close the door
-  elseif not self.connectedDoor and animator.animationState("doorState") == "open" then
+  elseif not self.connectedDoor and animator.animationState("doorState") == "open" and self.forceOpenTimer == 0 and not self.doorLocked then
 	animator.setAnimationState("doorState", "closing")
+	animator.playSound("close")
+  elseif animator.animationState("doorState") == "open" and self.doorLocked then
+	animator.setAnimationState("doorState", "closing")
+	animator.playSound("close")
   end
   
   --If we have a connected door and it still exists, make the door interactive
-  if self.connectedDoor and world.entityExists(self.connectedDoor) then
+  if self.connectedDoor and world.entityExists(self.connectedDoor) and not self.doorLocked then
 	object.setInteractive(true)
   else
 	object.setInteractive(false)
@@ -49,7 +70,8 @@ function update(dt)
   --Debug the detectArea and the stored door connection ID
   local detectPoly = {self.detectArea[1], {self.detectArea[1][1], self.detectArea[2][2]}, self.detectArea[2], {self.detectArea[2][1], self.detectArea[1][2]}}
   world.debugPoly(detectPoly, "cyan")
-  world.debugText(sb.print(self.connectedDoor), entity.position(), "yellow")
+  world.debugText("Lock: " .. sb.printJson(self.doorLocked), vec2.add(entity.position(), {0,1}), "yellow")
+  world.debugText("Link ID: " .. sb.printJson(self.connectedDoor), vec2.add(entity.position(), {0,0}), "yellow")
   world.debugPoint(world.entityMouthPosition(entity.id()), "cyan")
 end
 
--- stats/effects/thea-teleportentity/thea-teleportentity.lua
+++ stats/effects/thea-teleportentity/thea-teleportentity.lua
@@ -4,7 +4,7 @@ end
 
 function update(dt)
   --If we have a configured sourceEntity, and haven't teleported yet, do so now
-  if effect.sourceEntity() and world.entityExists(effect.sourceEntity()) and not self.teleported then
+  if effect.sourceEntity() and world.entityExists(effect.sourceEntity()) and not self.teleported and not status.statPositive("activeMovementAbilities") then
 	local targetPosition = world.entityMouthPosition(effect.sourceEntity())
 	
 	world.sendEntityMessage(effect.sourceEntity(), "openDoor")
@@ -12,8 +12,8 @@ function update(dt)
 	self.teleported = true
   end
   
-  --If we have already been teleported, expire the effect
-  if self.teleported then
+  --If we have already been teleported or are in an active movement ability, expire the effect
+  if self.teleported or status.statPositive("activeMovementAbilities") then
 	effect.expire()
   end
 end
